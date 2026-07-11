d6d6d6d8b4f1ea89b2809e9a1d3c0d74ea9a1657
Ryan Anderson <27966966+ryanpaulanderson@users.noreply.github.com>
2026-07-11 15:27:21 -0400

    chore: add project tooling and test stack

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  endOfLine: "lf",
+};
+
+export default config;
